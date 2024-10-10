35d6a6e08e5ae91c153d294b92e641805e537c74
xtxujun <54664034+xtxujun@users.noreply.github.com>
2024-10-10 19:25:29 +0800

    index.vue

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -17,8 +17,8 @@ const searchBoxHeight = '40px'
 
 <template>
   <TheDoc>
-     <div p="12 sm:24" bg="$main-bg-c" w="full sm:auto" :class="{ no_select: settingStore.isSetting }">
-   <MainHeader />
+    <div p="12 sm:24" bg="$main-bg-c" w="full sm:auto" :class="{ no_select: settingStore.isSetting }" style="height: 100vh">
+      <MainHeader />
       <MainClock v-if="!settingStore.isSetting" />
       <MainSearch v-if="!settingStore.isSetting" my-24 />
       <SiteContainer />
