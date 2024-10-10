5c4ecc01f650105d67ce6b10f1bbb806fce44f35
xtxujun <54664034+xtxujun@users.noreply.github.com>
2024-10-10 19:14:34 +0800

    index.vue

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,16 +1,3 @@
-<template>
-  <TheDoc>
-    <div p="12 sm:24" bg="$main-bg-c" w="full sm:auto" :class="{ no_select: settingStore.isSetting }" style="height: 100vh">
-      <MainHeader />
-      <MainClock v-if="!settingStore.isSetting" />
-      <MainSearch v-if="!settingStore.isSetting" my-24 :width="searchBoxWidth" :height="searchBoxHeight" />
-      <SiteContainer />
-      <MainSetting />
-      <TheFooter />
-    </div>
-  </TheDoc>
-</template>
-
 <script setup lang="ts">
 import MainHeader from './components/MainHeader.vue'
 import MainClock from './components/MainClock.vue'
@@ -24,20 +11,28 @@ defineOptions({
 
 const settingStore = useSettingStore()
 
-// 定义搜索框的宽度和高度
-const searchBoxWidth = '300px' // 你可以根据需要动态计算这个值
-const searchBoxHeight = '40px' // 你可以根据需要动态计算这个值
+const searchBoxWidth = '300px'
+const searchBoxHeight = '40px'
 </script>
 
-<route lang="json">
-{
-  "path": "/",
-  "children": [
-    {
-      "name": "setting",
-      "path": "setting",
+<template>
+  <TheDoc>
+    <div my="0 sm:6vh" p="12 sm:24" bg="$main-bg-c" w="full sm:auto" :class="{ no_select: settingStore.isSetting }">
+      <MainHeader />
+      <MainClock v-if="!settingStore.isSetting" />
+      <MainSearch v-if="!settingStore.isSetting" my-24 />
+      <SiteContainer :key="settingStore.siteContainerKey" />
+      <MainSetting />
+      <TheFooter v-if="settingStore.getSettingValue('showFooter')" />
+    </div>
+    <Blank />
+  </TheDoc>
+</template>
+
+<route lang="yaml">
+path: /
+children:
+  - name: setting
+    path: setting
     component: /src/components/Blank.vue
-    }
-  ]
-}
 </route>
